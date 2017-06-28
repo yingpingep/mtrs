a8679e4952dccee02cc6fa27d6d8741943639e86
yinnping <ciaoing@livemail.tw>
2017-06-28 19:50:02 +0800

    Add PingObject and HyperPing classes.

--- Project_mtrs/Program.cs
+++ Project_mtrs/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,23 @@ namespace Project_mtrs
     {
         static void Main(string[] args)
         {
+            
         }
+    }
+
+
+    [Serializable]
+    public class DestinationNotFoundException : Exception
+    {
+        public DestinationNotFoundException() { }
+        public DestinationNotFoundException(string message) : base(message) { }
+        public DestinationNotFoundException(string message, Exception inner) : base(message, inner) { }
+        public override string ToString()
+        {
+            return "Make sure enter an IP address or a host name.\n";
+        }
+        protected DestinationNotFoundException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
 }
